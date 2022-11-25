2b02564f5028423c86f393252cef88aca372e910
Joslynn Kim <joslynnkim@gmail.com>
2022-11-25 18:50:56 +0900

    1. 프로젝트 생성

---
+++ src/main/java/web/mvc/BlinkTownFinalApplication.java
@@ -0,0 +1,13 @@
+package web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkTownFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkTownFinalApplication.class, args);
+	}
+
+}
