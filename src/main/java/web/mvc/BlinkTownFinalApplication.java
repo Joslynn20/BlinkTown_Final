78dfab2dadedbc8eae323bf89255f049bf13efd6
egg <orchid59@naver.com>
2022-11-25 19:08:53 +0900

    first commit

--- src/main/java/web/mvc/BlinkTownFinalApplication.java
+++ src/main/java/web/mvc/BlinkTownFinalApplication.java
@@ -8,6 +8,7 @@ public class BlinkTownFinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlinkTownFinalApplication.class, args);
+		System.out.println("셋팅!!!!");
 	}
 
 }
