e28fa9d86aee6dcf55cae33b2bcf8594d6f79d70
Joslynn <joslynnkim@gmail.com>
2022-12-01 19:04:20 +0900

    김보경 #3 Product 상품 관리 Service, Repository 완성

--- src/main/java/web/mvc/domain/Album.java
+++ src/main/java/web/mvc/domain/Album.java
@@ -1,10 +1,10 @@
 package web.mvc.domain;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import javax.persistence.Entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,9 +13,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-public class Album extends Product{
+public class Album extends Product {
+
 	
+	
+
+	public Album(String productCode, String productName, String productEngName, int productPrice, int productStock,
+			String productSize, LocalDate productRegDate, String productMainImg, String productDetailImg,
+			int productReadNo, Category category, Integer productMembershipOnly, String albumComponent,
+			Date albumReleaseDate) {
+		super(productCode, productName, productEngName, productPrice, productStock, productSize, productRegDate,
+				productMainImg, productDetailImg, productReadNo, category, productMembershipOnly);
+		this.albumComponent = albumComponent;
+		this.albumReleaseDate = albumReleaseDate;
+	}
 	private String albumComponent;
 	private Date albumReleaseDate;
 
